b7aa2846251d4db0696778809dfa6d6d37954f54
michael <michael@MichaelPC>
2020-05-13 06:48:22 -0300

    TCC - Sistema de Gerenciamento Comercial

---
+++ src/Model/Produto.java
@@ -0,0 +1,51 @@
+
+package Model;
+
+
+public class Produto {
+    private int id;
+    private String nome;
+    private float preco;
+    private int quantidade;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
